efc65fc5a52f82a71c5c355e034a5c1649bdda8a
jdandrade <joao.andrade@aptoide.com>
2018-11-11 11:57:31 +0000

    set up new Android Navigation Component to handle all the app navigation.

---
+++ app/src/main/java/com/onedevarmy/appstoreconcept/AppStoreActivity.kt
@@ -0,0 +1,32 @@
+package com.onedevarmy.appstoreconcept
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_app_store.*
+
+class AppStoreActivity : AppCompatActivity() {
+
+    private lateinit var navigationController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_store)
+        setSupportActionBar(toolbar)
+
+        navigationController = Navigation.findNavController(this, R.id.app_navigation_fragment)
+        appBarConfiguration = AppBarConfiguration(navigationController.graph)
+
+        setupActionBarWithNavController(navigationController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigationController.navigateUp(appBarConfiguration)
+    }
+}
---
+++ app/src/main/java/com/onedevarmy/appstoreconcept/HomeFragment.kt
@@ -0,0 +1,5 @@
+package com.onedevarmy.appstoreconcept
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment()
