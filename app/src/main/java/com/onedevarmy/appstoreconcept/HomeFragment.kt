eee3b3079ac304d17d559a1f90134ab5c8dc033a
jdandrade <joao.andrade@aptoide.com>
2018-11-22 21:18:16 +0000

    fix: show home fragment correctly with the navigation component.

--- app/src/main/java/com/onedevarmy/appstoreconcept/HomeFragment.kt
+++ app/src/main/java/com/onedevarmy/appstoreconcept/HomeFragment.kt
@@ -1,5 +1,18 @@
 package com.onedevarmy.appstoreconcept
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class HomeFragment : Fragment()
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
